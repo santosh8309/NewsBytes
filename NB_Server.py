8ec696aef9c36255dc02d4069b2328aebf5868de
Jayasilan <jayasilan.j@algonox.com>
2020-08-13 22:50:09 +0530

    first commit

---
+++ NB_Server.py
@@ -0,0 +1,15 @@
+from flask import Flask
+ 
+app = Flask(__name__)
+ 
+@app.route('/')
+def main():
+    print("Hello! You have arrived at a default page!")
+ 
+@app.route('/NewsBytes')
+def Encode_():
+	print("Hello, Santosh!")
+
+if __name__ == '__main__':
+
+	app.run(debug=True)
